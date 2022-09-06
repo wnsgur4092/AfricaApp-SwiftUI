6e350cf0c6dc9a97b416c2826eb1d480e25488a4
Jun <wnsgur4092@gmail.com>
2022-09-06 18:39:01 +1000

    #2. [Feat] CoverImageView
    - Fetch covers.json data and load it in CoverImageView
    - Update ContentView
    - Update Codable Bundle Extension

---
+++ AfricaApp-SwiftUI/App (V)/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  AfricaApp-SwiftUI
+//
+//  Created by JunHyuk Lim on 6/9/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    //MARK: - PROPERTIES
+    
+    //MARK: - BODY
+    var body: some View {
+        NavigationView {
+            List {
+                CoverImageView()
+                    .frame(height: 300)
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            } //: LIST
+            .navigationTitle("Africa")
+            .navigationBarTitleDisplayMode(.large)
+        } //: NAVIGATION
+    }
+}
+
+//MARK: - PREVIEW
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ AfricaApp-SwiftUI/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  AfricaApp-SwiftUI
+//
+//  Created by JunHyuk Lim on 6/9/2022.
+//
+
+import Foundation
+
+struct CoverImage : Codable, Identifiable {
+    let id : Int
+    let name : String
+}
