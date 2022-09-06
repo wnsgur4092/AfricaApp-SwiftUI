e48027790a0d91d16e84c1140bd6c5888bda4c8e
Jun <wnsgur4092@gmail.com>
2022-09-06 16:24:06 +1000

    Initial Commit

---
+++ AfricaApp-SwiftUI/AfricaApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp_SwiftUIApp.swift
+//  AfricaApp-SwiftUI
+//
+//  Created by JunHyuk Lim on 6/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
